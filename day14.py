9320e92e0bc84a33c4faab0550f81f4b2f662d80
DanyaRdm <daniil_rubtsov@internet.ru>
2025-11-03 15:36:11 +0400

    Create day14.py

---
+++ day14.py
@@ -0,0 +1,32 @@
+a = int(input())  # 1
+b = int(input())  # 2
+c = (a + b) ** 2
+n = a ** 2 + b ** 2
+print("Квадрат суммы", a, "и", b, 'равен', c)
+print("Сумма квадратов", a, "и", b, 'равна', n)
+
+# На входе данные 5 должно выдать 5 + 55 + 555 
+a = int(input())  # 1
+b = str(a) + str(a)
+c = str(a) + str(a) + str(a)
+n = int(b) + int(c) + int(a)
+print(n)
+
+
+a = int(input())  # 1423
+b = a // 1000  # 1
+c = (a // 100) % 10  # 4 
+n = (a % 100) // 10  # 2
+m = a % 10  # 3
+if b + m == c - n:
+    print("ДА")
+else:
+    print('НЕТ')
+
+# Функции min и max:
+a = int(input())  # 1
+b = int(input())  # 2
+c = int(input())  # 3
+n = int(input())  # 4
+print(min(a,b,c,n))  # 1
+print(max(a,b,c,n))  # 4

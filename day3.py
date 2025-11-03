146f80d90a53003cd979e151a95b30658001e2c5
DanyaRdm <daniil_rubtsov@internet.ru>
2025-11-03 15:36:37 +0400

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,7 @@
+a = int(input("Введите 1 число"))
+b = int(input("Введите 2 число"))
+print("Сумма", a + b)
+
+z = float(input("ВВедите число 1"))
+x = float(input("ВВедите число 2"))
+print("Сумма", z + x)
